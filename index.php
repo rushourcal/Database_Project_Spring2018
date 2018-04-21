8486389572b5161aef50ca1528e439f57bd21b62
Zach4550 <zachariah.boone@mavs.uta.edu>
2018-04-20 20:17:27 -0500

    Added base of add Customer page, updated some other stuff I may have been incorrect about.

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <body>
 <h1>Options:</h1>
 <a href="addAuthor.php">Add Author</a><!--this is a simple link to the addAuthor.php file-->
+<a href="addCustomer.php">Add Author</a>
 <p>More soon</p>
 </body>
 </title>
