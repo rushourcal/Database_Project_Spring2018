2082a7d9b18580b7682585d0bc28cc449a7e0108
Zach4550 <zachariah.boone@mavs.uta.edu>
2018-04-22 18:44:49 -0500

    fixing index again.

--- index.php
+++ index.php
@@ -6,7 +6,6 @@
 <p><a href="addItem.php">Add Item</a></p>
 <p><a href="addOrder.php">Add Order</a></p>
 <p><a href="adjustItems.php">Change Item</a></p>
-<p><a href="orderlookup.php">Order Lookup</a></p>
 <p><a href="itemsSold.php">Item Sales</a></p>
 <p><a href="orderLookup.php">Lookup Order</a></p>
 <p><a href="customerLookup.php">Lookup Customer Purchases</a></p>
